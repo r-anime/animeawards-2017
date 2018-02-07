e01e3d8d22f9a0c9a8850312d16382d35a9c6258
George <georgej1088@gmail.com>
2018-02-07 13:09:26 +0000

    Workaround for my preview environment being retarded

--- awardsdata.js
+++ awardsdata.js
@@ -1,9 +1,11 @@
 var app = angular.module('aniwardsApp', []);
 
 app.controller('aniwardsController', function($scope, $http) {
-    $http.get("http://geo1088.me/animeawards/awards.json").then(function(response) {
-		$scope.awards = response.data.awards;
-		$scope.$broadcast('dataloaded');
-		//console.log($scope.awards);
-	});
+	// $http.get("https://geo1088.github.io/animeawards/awards.json").then(function(response) {
+	//	$scope.awards = response.data.awards;
+	//	$scope.$broadcast('dataloaded');
+	//	//console.log($scope.awards);
+	// });
+	$scope.awards = stuff.awards;
+	$scope.$broadcast('dataloaded');
 });
